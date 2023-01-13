38ee8afdb200e2063ae532e7c33bf449fec8de68
Mateusz <mgmateuszgalazka860@gmail.com>
2023-01-13 19:27:33 +0100

    Revert "Cleanup"
    This reverts commit 197f81275fe6c73181d9635579be5b2cd11b086f.

--- Tables/Aktor.sql
+++ Tables/Aktor.sql
@@ -1,6 +1,6 @@
-CREATE TABLE dbo.Aktor (
-    AktorID INT IDENTITY (1, 1) NOT NULL,
-    OsobaID INT NOT NULL,
+CREATE TABLE [dbo].[Aktor] (
+    [AktorID] INT IDENTITY (1, 1) NOT NULL,
+    [OsobaID] INT NOT NULL,
     PRIMARY KEY CLUSTERED ([AktorID] ASC),
     CONSTRAINT [FK_Aktorzy_Osoby] FOREIGN KEY ([OsobaID]) REFERENCES [dbo].[Osoba] ([OsobaID])
 );
